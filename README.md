e4e1a190992b904cfbc7ccd9eece5e971d31112d
Martin Meyer <martin.meyer@inftec.ch>
2021-04-09 15:14:05 +0200

    Connecting to CosmosDB using mongo driver

--- README.md
+++ README.md
@@ -25,3 +25,17 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Azure Functions
+
+### Development
+
+* To run in **watch mode**, run two processes:
+
+  1. `npm run watch`
+  2. `npm start`
+
+### Configuration Cloud
+
+* Add env variable to configuration:
+  * *APP_MONGO_URL*=mongodb://inftec-mme-poc-comosdb:...
--- api/GetMessage/index.ts
+++ api/GetMessage/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { MongoClient } from "mongodb";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -7,12 +8,34 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    const mongoUrl = process.env.APP_MONGO_URL;
+    console.log('from process: ', mongoUrl);
+
+    const client = new MongoClient(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    let text;
+    try {
+      await client.connect();
+      const database = client.db('mme-test');
+      const foo = database.collection('foo');
+      // Query for a movie that has the title 'Back to the Future'
+      const query = {};// { title: 'Back to the Future' };
+      const res = await foo.findOne(query);
+      console.log('result', res);
+      text = "Hello from the API " + JSON.stringify(res);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+
     context.res = {
         body: {
-          text: "Hello from the API"
+          text
         }
     };
-
 };
 
 export default httpTrigger;
