b254a47d55afba2cbd72b26aaec905bf0a0b2315
Martin Meyer <martin.meyer@inftec.ch>
2021-04-22 12:11:42 +0200

    Verifying ID token

--- api/testing-manual/token-validation.test.ts
+++ api/testing-manual/token-validation.test.ts
@@ -11,24 +11,39 @@ const fetch = require('node-fetch');
 const token = process.env.TOKEN;
 
 if (!token) {
-  throw new Error("Please set env variable TOKEN");
+  throw new Error("Please set env variable TOKEN to contain the ID token to verify");
 }
 
-console.log("Decoding...", decode(token));
+const {header, payload} = decode(token, {complete: true});
+console.log("Decoded:", header, payload);
 
-const openidConfUrl = "https://login.microsoftonline.com/5a35dd12-2027-4c00-b63a-cc5076fec4b9/v2.0/.well-known/openid-configuration";
+//const openidConfUrl = "https://login.microsoftonline.com/5a35dd12-2027-4c00-b63a-cc5076fec4b9/v2.0/.well-known/openid-configuration";
+// # ID for ics-portal
+const openidConfUrl = "https://login.microsoftonline.com/0555ccbb-73e1-4617-aea4-74c63bef3918/v2.0/.well-known/openid-configuration";
 
-console.log("Verifying...");
+const kid = header.kid;
 
-fetch(openidConfUrl)
+console.log("Verifying for KID", kid);
+
+async function doVerify() {
+  const config = await fetch(openidConfUrl)
     .then(res => res.text())
-    .then(body => JSON.parse(body))
-    .then(config => fetch(config.jwks_uri))
+    .then(body => JSON.parse(body));
+
+  const jwksUri = config.jwks_uri;
+  const jwks = await fetch(jwksUri)
     .then(res => res.text())
-    .then(body => JSON.parse(body))
-    .then(keys => console.log(keys));
+    .then(body => JSON.parse(body));
+  const keys = jwks.keys;
+  console.log('##keys', keys);
+  const signingKey = keys.find(key => key.kid === kid);
+  console.log('##signingKey', signingKey);
 
-// console.log("Getting public key");
+  const key = `-----BEGIN CERTIFICATE-----\n${signingKey.x5c[0]}\n-----END CERTIFICATE-----`
+  console.log('##key', key);
 
+  console.log("Verifying...", verify(token, key));//, {algorithms: ['RS256']}));
+}
 
-// console.log("Verifying...", verify(token));
+doVerify();
+
